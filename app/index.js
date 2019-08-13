8951ec0fa35a64320686a48e0a44c6db273b8f7d
Binu Tomy <binu_tomy@hotmail.co.uk>
2019-08-13 12:13:28 +0100

    react lists with key

--- app/index.js
+++ app/index.js
@@ -3,32 +3,29 @@ import ReactDOM from "react-dom";
 
 import './index.css';
 
-
-const tweets = [
-  { id: 1, stars: 13, text: 'Turns out "git reset --hard HEAD^" was a terrible idea.' },
-  { id: 2, stars: 87, text: 'Tech conferences are too expensive.' },
-  { id: 3, stars: 51, text: 'Clean code is subjective. Optimize for deletion.' },
-  { id: 4, stars: 19, text: 'Maybe the real benefit of open source was the friendships we made along the way?' },
-]
-
-
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <ul>
-          {tweets.map((tweet) => (
-            <li key={tweet.id}>
-              {tweet.text}
-            </li>
-          ))}
-        </ul>
-      </React.Fragment>
+
+      <ul>
+        {this.props.friends.map((friend) => (
+          <li key={friend.id}>
+            {friend.name}
+          </li>
+        ))}
+      </ul>
+
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <App friends={[
+    { id: 893, name: 'Mikenzi' },
+    { id: 871, name: 'Cash' },
+    { id: 982, name: 'Steven' },
+    { id: 61, name: 'Kimmy' },
+    { id: 117, name: 'Doug' }
+  ]} />,
   document.getElementById('app')
 )

69a0adc9a54369c48db52d5a0e271ea7238e8449
Binu Tomy <binu_tomy@hotmail.co.uk>
2019-08-13 11:27:53 +0100

    conditional rendering in jsx

--- app/index.js
+++ app/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 
 import './index.css';
 
+
+function showWarning() {
+  return true;
+}
+
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World!
-      </div>
+      <React.Fragment>
+        {
+          showWarning() === true && <h3>Hello World!</h3>
+        }
+      </React.Fragment>
     )
   }
 }

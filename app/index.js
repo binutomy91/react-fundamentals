802f64bab3923d41de170aae1aa4416ebc1de907
Binu Tomy <binu_tomy@hotmail.co.uk>
2019-08-13 11:46:06 +0100

    props in react

--- app/index.js
+++ app/index.js
@@ -3,24 +3,52 @@ import ReactDOM from "react-dom";
 
 import './index.css';
 
-
-function showWarning() {
-  return true;
+var USER_DATA = {
+  name: 'Tyler McGinnis',
+  img: 'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460',
+  username: 'tylermcginnis'
 }
+
+
+class Avatar extends React.Component {
+  render() {
+    return (
+      <img src={this.props.img} />
+    )
+  }
+}
+
+class Label extends React.Component {
+  render() {
+    return (
+      <h1>Name: {this.props.name}</h1>
+    )
+  }
+}
+
+class ScreenName extends React.Component {
+  render() {
+    return (
+      <h3>Username: {this.props.username}</h3>
+    )
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        {
-          showWarning() === true && <h3>Hello World!</h3>
-        }
+         <Avatar img={this.props.user.img} />
+        <Label name={this.props.user.name} />
+        <ScreenName username={this.props.user.username}/>
       </React.Fragment>
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <App user={USER_DATA}
+  />,
   document.getElementById('app')
 )
